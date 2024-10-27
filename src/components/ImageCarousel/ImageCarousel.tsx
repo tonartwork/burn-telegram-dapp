6da9dd24593e7defe503b727ed63ac05731375b9
ilyaviache <ilya.viache@gmail.com>
2024-10-27 22:31:16 +0700

    add swipe carousel

--- src/components/ImageCarousel/ImageCarousel.tsx
+++ src/components/ImageCarousel/ImageCarousel.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import Image from 'next/image';
 import { cn } from "@/lib/utils";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/Carousel";
 
 interface ImageCarouselProps {
   onSelect: (index: number) => void;
@@ -8,22 +15,22 @@ interface ImageCarouselProps {
 }
 
 export const ImageCarousel: React.FC<ImageCarouselProps> = ({ onSelect, selectedImage }) => {
-  const images = Array(9).fill('/images/guardiance-image.png');
+  const images = Array(18).fill('/images/guardiance-image.png');
 
-  return (
+  const renderGrid = (startIndex: number) => (
     <div className="grid grid-cols-3 gap-2">
-      {images.map((src, index) => (
+      {images.slice(startIndex, startIndex + 9).map((src, index) => (
         <div 
-          key={index} 
+          key={startIndex + index} 
           className={cn(
             "relative cursor-pointer",
-            selectedImage === index && "ring-2 ring-green-500"
+            selectedImage === startIndex + index && "ring-2 ring-green-500"
           )}
-          onClick={() => onSelect(index)}
+          onClick={() => onSelect(startIndex + index)}
         >
           <Image
             src={src}
-            alt={`NFT ${index + 1}`}
+            alt={`NFT ${startIndex + index + 1}`}
             width={100}
             height={100}
             className="w-full rounded-xl"
@@ -31,5 +38,26 @@ export const ImageCarousel: React.FC<ImageCarouselProps> = ({ onSelect, selected
         </div>
       ))}
     </div>
+  );
+
+  return (
+    <Carousel
+      opts={{
+        align: "start",
+        loop: true,
+      }}
+      className="w-full"
+      setSelectedIndex={(index) => {}}
+    >
+      <CarouselContent>
+        {[0, 9].map((startIndex) => (
+          <CarouselItem key={startIndex}>
+            {renderGrid(startIndex)}
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
   );
 };
