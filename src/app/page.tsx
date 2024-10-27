880edbabb17695585cddcd80593e4d267c5cccbe
ilyaviache <ilya.viache@gmail.com>
2024-10-27 23:46:31 +0700

    add wallet connect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
 'use client';
 
+import React, { forwardRef, ForwardedRef, useRef } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/Card';
 import { Button } from '@/components/ui/Button';
 import { Page } from '@/components/Page';
+import { TonConnectButton, useTonWallet, TonConnectButtonProps, useTonConnectUI } from '@tonconnect/ui-react';
+
+TonConnectButton.displayName = 'TonConnectButton';
 
 export default function Home() {
+  const wallet = useTonWallet();
+  const [ tonConnectUI ] = useTonConnectUI();
+
   return (
     <Page back={false}>
       <div className="container mx-auto px-4 py-8 bg-white text-mainText">
@@ -23,19 +30,25 @@ export default function Home() {
               width={300}
               height={300}
               className="w-full rounded-xl"
+              priority
             />
           </CardContent>
           <CardFooter>
-            <Button className="w-full bg-black text-white hover:bg-gray-800">Connect Wallet</Button>
+            <Button 
+              className="w-full bg-black text-white hover:bg-gray-800"
+              onClick={() => tonConnectUI.openModal()}
+            >
+              Connect Wallet
+            </Button>
           </CardFooter>
         </Card>
         <Link href="/collection" passHref>
-            <Button className="w-full mt-4 bg-gray-200 text-black hover:bg-gray-300">
-              View Collection (for testing only)
-            </Button>
-          </Link>
+          <Button className="w-full mt-4 bg-gray-200 text-black hover:bg-gray-300">
+            View Collection (for testing only)
+          </Button>
+        </Link>
         <p className="text-center text-sm text-gray-600 mb-8">
-          Connect your wallet to buy and mint NFT
+          {wallet ? 'Wallet connected' : 'Connect your wallet to buy and mint NFT'}
         </p>
       </div>
     </Page>
