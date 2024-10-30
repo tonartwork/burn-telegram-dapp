e4935ea9291febdbed32ce202eaadcc5bf88195f
Pavel Listopad <samp1etex7@gmail.com>
2024-10-30 23:10:59 +0700

    Add useTonClient hook with toncenterapi to communicate with blockchain

--- src/core/config/env.ts
+++ src/core/config/env.ts
@@ -5,6 +5,7 @@ export interface EnvVariables {
   NEXT_PUBLIC_TONAPI_KEY?: string
   NEXT_PUBLIC_COLLECTION_ADDRESS: string
   NEXT_PUBLIC_BURN_CONTRACT_ADDRESS: string
+  NEXT_PUBLIC_TONCENTER_MAINNET_KEY: string
 }
 
 // Export environment variables
@@ -14,6 +15,8 @@ export const env = {
   NEXT_PUBLIC_TONAPI_KEY: process.env.NEXT_PUBLIC_TONAPI_KEY || '',
   NEXT_PUBLIC_COLLECTION_ADDRESS: process.env.NEXT_PUBLIC_COLLECTION_ADDRESS || '',
   NEXT_PUBLIC_BURN_CONTRACT_ADDRESS: process.env.NEXT_PUBLIC_BURN_CONTRACT_ADDRESS || '',
+  NEXT_PUBLIC_TONCENTER_MAINNET_KEY: process.env.NEXT_PUBLIC_TONCENTER_MAINNET_KEY || '',
+  NEXT_PUBLIC_TONCENTER_TESTNET_KEY: process.env.NEXT_PUBLIC_TONCENTER_TESTNET_KEY || '',
 } as const;
 
 // Only validate in development
@@ -21,6 +24,9 @@ if (process.env.NODE_ENV === 'development') {
   const requiredEnvs = [
     'NEXT_PUBLIC_COLLECTION_ADDRESS',
     'NEXT_PUBLIC_BURN_CONTRACT_ADDRESS',
+    'NEXT_PUBLIC_TONAPI_KEY',
+    'NEXT_PUBLIC_TONCENTER_MAINNET_KEY',
+    'NEXT_PUBLIC_TONCENTER_TESTNET_KEY',
   ] as const;
 
   for (const key of requiredEnvs) {
---
+++ src/hooks/useTonClient.ts
@@ -0,0 +1,34 @@
+import { getHttpEndpoint } from "@orbs-network/ton-access";
+import { CHAIN } from "@tonconnect/ui-react";
+
+import { useAsyncInitialize } from "@/hooks/useAsyncInitialize";
+import { useTonConnect } from "@/hooks/useTonConnect";
+import { TonClient } from "@ton/ton";
+import { env } from '@/core/config/env';
+
+export function useTonClient() {
+    const {network} = useTonConnect()
+    const apiKey = network === CHAIN.MAINNET ? env.NEXT_PUBLIC_TONCENTER_MAINNET_KEY : env.NEXT_PUBLIC_TONCENTER_TESTNET_KEY;
+    
+    console.log('network', network);
+    return {
+        client: useAsyncInitialize(async ()=>{
+            if(!network) return;
+
+            return new TonClient({
+                endpoint: await getHttpEndpoint({
+                    network: network === CHAIN.MAINNET ? "mainnet" : "testnet"
+                }),
+                apiKey
+            })
+        }, [network]),
+        publicClient: useAsyncInitialize(async ()=>{
+            return new TonClient({
+                endpoint: await getHttpEndpoint({
+                    network: network === CHAIN.MAINNET ? "mainnet" : "testnet"
+                }),
+                apiKey
+            })
+        }, [network])
+    }
+}
